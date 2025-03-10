1ec473a3f8fead30fe93da0a3a923bcf3959999a
Bram De Coster <bram.decoster@student.hogent.be>
2025-03-10 10:11:31 +0100

    added edit icon

--- src/components/DailyList.js
+++ src/components/DailyList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 const DailyList = ({ dailyData, onClickEdit }) => {
   return (
@@ -6,10 +8,10 @@ const DailyList = ({ dailyData, onClickEdit }) => {
       {dailyData.entries.length > 0 ? (
         dailyData.entries.map((entry) => (
           <li key={entry.id} className="food-item">
-            {entry.amount}x <strong>{entry.name}</strong> (
+            {entry.amount}x <strong> {entry.name}</strong> (
             {parseFloat(entry.total_kcal.toFixed(1))} kcal)
             <button onClick={() => onClickEdit(entry.food_id, entry.name)}>
-              Edit
+              <FontAwesomeIcon icon={faEdit} />
             </button>
           </li>
         ))

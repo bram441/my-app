d127b6a9d884d9496ceb181d8e5b65f4b884e152
Bram De Coster <bram.decoster@student.hogent.be>
2025-03-26 14:59:51 +0100

    fixed foodlist for recipes

--- src/components/recipes/RecipeFoodList.js
+++ src/components/recipes/RecipeFoodList.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../css/global.css";
+import "../css/recipes.css";
 
 const RecipeFoodList = ({ foods }) => {
   return (
-    <div className="food-list">
+    <div className="food-list recipe-food-list">
       {foods.length > 0 ? (
         foods.map((food) => (
           <div key={food.id} className="food-item">
             <div className="food-name">
-              <h3>{food.name}</h3>
+              <h4>{food.name}</h4>
             </div>
             <div className="food-kcal">{food.RecipeFood.quantity} gr/ml</div>
           </div>

870d170fb7b19cc0e3698511c3774ad0df717c2f
Bram De Coster <bram.decoster@student.hogent.be>
2025-04-17 10:40:30 +0200

    front camera updated

--- src/components/scanner/Camera.js
+++ src/components/scanner/Camera.js
@@ -1,20 +1,21 @@
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import Tesseract from "tesseract.js";
+import CameraPhoto, { isMobile } from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 
 const Camera = ({ onExtractedText }) => {
   const webcamRef = useRef(null);
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  // Video constraints to use the back camera
-  const videoConstraints = {
-    facingMode: "environment", // Use the back camera
-  };
-
-  const captureImage = () => {
+  const captureImageDesktop = () => {
     const capturedImage = webcamRef.current.getScreenshot();
     setImage(capturedImage);
+  };
+
+  const captureImageMobile = (dataUri) => {
+    setImage(dataUri);
   };
 
   const extractText = async () => {
@@ -37,14 +38,29 @@ const Camera = ({ onExtractedText }) => {
     <div>
       {!image ? (
         <div>
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            width={400}
-            videoConstraints={videoConstraints} // Set video constraints
-          />
-          <button onClick={captureImage}>Capture Image</button>
+          {isMobile() ? (
+            <CameraPhoto
+              onTakePhoto={(dataUri) => captureImageMobile(dataUri)}
+              idealFacingMode="environment" // Use back camera
+              isImageMirror={false} // Prevent mirroring
+              idealResolution={{ width: 1920, height: 1080 }}
+              imageType="image/jpeg"
+              imageCompression={0.9}
+            />
+          ) : (
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              width={400}
+              videoConstraints={{
+                facingMode: "environment", // Use back camera
+              }}
+            />
+          )}
+          <button onClick={isMobile() ? null : captureImageDesktop}>
+            Capture Image
+          </button>
         </div>
       ) : (
         <div>
